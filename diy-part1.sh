6317a94e84f65a5a16bd9e4bce826f759e6b876c
polarcue <112614345+polarcue@users.noreply.github.com>
2024-10-04 17:16:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git ua2f https://github.com/Zxilly/UA2F.git' >>feeds.conf.default
